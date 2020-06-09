7e4c965f1ae8abacf4b8bc228b10517523d22323
César López Ramírez <cesar.lopez@coopdevs.org>
2020-06-09 17:44:12 +0200

    Initial commit

---
+++ somconnexio/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Odoo Som Connexió customizations",
+    'version': '12.0.0.0.1',
+    'depends': ['easy_my_coop_es'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo Som Connexió customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+    ],
+}
