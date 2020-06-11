0efdf76b3e7a5dd2f769a67fe0c15ece81088380
Daniel Palomar <danypr92@gmail.com>
2020-06-11 18:35:05 +0200

    Fix flake8 warnings

--- somconnexio/models/coop.py
+++ somconnexio/models/coop.py
@@ -6,14 +6,14 @@ from odoo.exceptions import UserError
 class SubscriptionRequest(models.Model):
     _inherit = 'subscription.request'
     share_product_id = fields.Many2one(required=False)
-    type = fields.Selection(selection_add=[('refered','Refered')])
+    type = fields.Selection(selection_add=[('referred', 'Referred')])
 
     @api.one
     def validate_subscription_request(self):
         try:
             invoice = super().validate_subscription_request()
         except UserError:
-            if self.ordered_parts == 0 and self.type=='refered':
+            if self.ordered_parts == 0 and self.type == 'referred':
                 pass
                 raise # Not implemented yet
             else:
