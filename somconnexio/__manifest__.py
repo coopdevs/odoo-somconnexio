ec272cee63d7edabb92cd12bb26da85ab957505f
Daniel Palomar <danypr92@gmail.com>
2020-06-19 12:34:07 +0200

    Add as depends the OCA modules to improve the UI

--- somconnexio/__manifest__.py
+++ somconnexio/__manifest__.py
@@ -3,7 +3,12 @@
     'version': '12.0.0.0.1',
     'depends': [
         'easy_my_coop_es',
-        'easy_my_coop_sponsorship'
+        'easy_my_coop_sponsorship',
+        'web_favicon',
+        'web_responsive',
+        'web_decimal_numpad_dot',
+        'web_no_bubble',
+        'web_searchbar_full_width'
     ],
     'author': "Coopdevs Treball SCCL",
     'website': 'https://coopdevs.org',
