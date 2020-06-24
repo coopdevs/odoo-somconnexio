1c883509fa50552fe530e4d3bd26ef42cf65dda3
César López Ramírez <cesar.lopez@coopdevs.org>
2020-06-24 12:53:30 +0200

    last_subscription_request added

--- somconnexio/__manifest__.py
+++ somconnexio/__manifest__.py
@@ -1,9 +1,11 @@
 {
     'name': "Odoo Som Connexió customizations",
-    'version': '12.0.0.0.1',
+    'version': '12.0.0.0.3',
     'depends': [
         'easy_my_coop_es',
-        'easy_my_coop_sponsorship'
+        'easy_my_coop_sponsorship',
+        'sale_management',
+        'contract'
     ],
     'author': "Coopdevs Treball SCCL",
     'website': 'https://coopdevs.org',
@@ -12,5 +14,7 @@
     Odoo Som Connexió customizations.
     """,
     "license": "AGPL-3",
-    'data': [],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
 }
--- somconnexio/models/__init__.py
+++ somconnexio/models/__init__.py
@@ -1,1 +1,2 @@
 from . import subscription_request
+from . import res_partner
