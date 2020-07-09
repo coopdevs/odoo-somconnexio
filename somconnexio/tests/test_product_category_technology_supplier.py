cdf45d5d3ef1ef10bcbecb8af0a499dfb7094dcb
César López Ramírez <cesar.lopez@coopdevs.org>
2020-07-09 09:55:03 +0200

    Tests for wrong tech, supplier and product

--- somconnexio/tests/test_product_category_technology_supplier.py
+++ somconnexio/tests/test_product_category_technology_supplier.py
@@ -38,9 +38,6 @@ class ProductCategoryTechnologySupplier(TransactionCase):
             'service_technology_id': self.ref(
                 'somconnexio.service_technology_fiber'
             ),
-            'service_supplier_id': self.ref(
-                'somconnexio.service_supplier_vodafone'
-            ),
             'partner_id': self.ref(
                 'easy_my_coop.res_partner_cooperator_2_demo'
             ),
@@ -119,10 +116,22 @@ class ProductCategoryTechnologySupplier(TransactionCase):
             mobile_additional_product_tmpl.product_variant_id
         )
 
+        broadband_additional_product_tmpl_args = {
+            'name': 'IPv4 Fixa',
+            'type': 'service',
+            'categ_id': self.ref('somconnexio.broadband_additional_service')
+        }
+        broadband_additional_product_tmpl = self.env['product.template'].create(
+            broadband_additional_product_tmpl_args
+        )
+        self.product_broadband_additional = (
+            broadband_additional_product_tmpl.product_variant_id
+        )
+
     def test_contract_adsl_orange_wrong_product(self):
         contract_adsl_wrong_product_args = self.contract_adsl_args.copy()
         contract_adsl_wrong_product_args['contract_line_ids'] = [(0, False, {
-            "name": "ADSL",
+            "name": "Fiber",
             "product_id": self.product_broadband_fiber.id
         })]
         self.assertRaises(
@@ -142,6 +151,9 @@ class ProductCategoryTechnologySupplier(TransactionCase):
         }), (0, False, {
             "name": "Alta parell existent a terminis",
             "product_id": self.product_broadband_adsl_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
         })]
         self.assertRaises(
             ValidationError,
@@ -160,6 +172,9 @@ class ProductCategoryTechnologySupplier(TransactionCase):
         }), (0, False, {
             "name": "Alta parell existent a terminis",
             "product_id": self.product_broadband_adsl_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
         })]
         self.assertRaises(
             ValidationError,
@@ -176,6 +191,9 @@ class ProductCategoryTechnologySupplier(TransactionCase):
         }), (0, False, {
             "name": "Alta parell existent a terminis",
             "product_id": self.product_broadband_adsl_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
         })]
         self.assertTrue(
             self.env['contract.contract'].create(contract_adsl_allowed_product_args)
@@ -184,7 +202,7 @@ class ProductCategoryTechnologySupplier(TransactionCase):
     def test_contract_mobile_wrong_product(self):
         contract_mobile_wrong_product_args = self.contract_mobile_args.copy()
         contract_mobile_wrong_product_args['contract_line_ids'] = [(0, False, {
-            "name": "Mobile Sense Minuts",
+            "name": "ADSL 20Mb",
             "product_id": self.product_broadband_adsl.id
         })]
         self.assertRaises(
@@ -209,6 +227,72 @@ class ProductCategoryTechnologySupplier(TransactionCase):
             contract_mobile_allowed_product_args
         ))
 
+    def test_contract_mobile_wrong_technology(self):
+        contract_mobile_wrong_tech_args = self.contract_mobile_args.copy()
+        contract_mobile_wrong_tech_args['service_technology_id'] = (
+            self.ref("somconnexio.service_technology_adsl")
+        )
+        contract_mobile_wrong_tech_args['contract_line_ids'] = [(0, False, {
+            "name": "Mobile Sense Minuts",
+            "product_id": self.product_mobile.id
+        }), (0, False, {
+            "name": "One Shot Mobile",
+            "product_id": self.product_mobile_oneshot.id
+        }), (0, False, {
+            "name": "Internacional 100 Min",
+            "product_id": self.product_mobile_additional.id
+        })]
+        self.assertRaises(
+            ValidationError,
+            self.env['contract.contract'].create,
+            [contract_mobile_wrong_tech_args]
+        )
+
+    def test_contract_mobile_wrong_supplier(self):
+        contract_mobile_wrong_supplier_args = self.contract_mobile_args.copy()
+        contract_mobile_wrong_supplier_args['service_supplier_id'] = (
+            self.ref("somconnexio.service_supplier_orange")
+        )
+        contract_mobile_wrong_supplier_args['contract_line_ids'] = [(0, False, {
+            "name": "Mobile Sense Minuts",
+            "product_id": self.product_mobile.id
+        }), (0, False, {
+            "name": "One Shot Mobile",
+            "product_id": self.product_mobile_oneshot.id
+        }), (0, False, {
+            "name": "Internacional 100 Min",
+            "product_id": self.product_mobile_additional.id
+        })]
+        self.assertRaises(
+            ValidationError,
+            self.env['contract.contract'].create,
+            [contract_mobile_wrong_supplier_args]
+        )
+
+    def test_contract_fiber_orange_wrong_tech_product(self):
+        contract_fiber_wrong_tech_args = self.contract_fiber_args.copy()
+        contract_fiber_wrong_tech_args['service_supplier_id'] = self.ref(
+            'somconnexio.service_supplier_orange'
+        )
+        contract_fiber_wrong_tech_args['service_technology_id'] = self.ref(
+            'somconnexio.service_technology_adsl'
+        )
+        contract_fiber_wrong_tech_args['contract_line_ids'] = [(0, False, {
+            "name": "Fiber",
+            "product_id": self.product_broadband_fiber.id
+        }), (0, False, {
+            "name": "One Shot Broadband",
+            "product_id": self.product_broadband_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
+        })]
+        self.assertRaises(
+            ValidationError,
+            self.env['contract.contract'].create,
+            [contract_fiber_wrong_tech_args]
+        )
+
     def test_contract_fiber_orange_allowed_product(self):
         contract_fiber_allowed_product_args = self.contract_fiber_args.copy()
         contract_fiber_allowed_product_args['service_supplier_id'] = self.ref(
@@ -220,6 +304,9 @@ class ProductCategoryTechnologySupplier(TransactionCase):
         }), (0, False, {
             "name": "One Shot Broadband",
             "product_id": self.product_broadband_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
         })]
         self.assertTrue(
             self.env['contract.contract'].create(contract_fiber_allowed_product_args)
@@ -231,7 +318,7 @@ class ProductCategoryTechnologySupplier(TransactionCase):
             'somconnexio.service_supplier_orange'
         )
         contract_fiber_wrong_product_args['contract_line_ids'] = [(0, False, {
-            "name": "Fiber",
+            "name": "ADSL 20Mb",
             "product_id": self.product_broadband_adsl.id
         })]
         self.assertRaises(
@@ -240,14 +327,65 @@ class ProductCategoryTechnologySupplier(TransactionCase):
             [contract_fiber_wrong_product_args]
         )
 
+    def test_contract_fiber_vodafone_wrong_tech_product(self):
+        contract_fiber_wrong_tech_args = self.contract_fiber_args.copy()
+        contract_fiber_wrong_tech_args['service_technology_id'] = self.ref(
+            'somconnexio.service_technology_adsl'
+        )
+        contract_fiber_wrong_tech_args['service_supplier_id'] = self.ref(
+            'somconnexio.service_supplier_vodafone'
+        )
+        contract_fiber_wrong_tech_args['contract_line_ids'] = [(0, False, {
+            "name": "Fiber",
+            "product_id": self.product_broadband_fiber.id
+        }), (0, False, {
+            "name": "One Shot Broadband",
+            "product_id": self.product_broadband_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
+        })]
+        self.assertRaises(
+            ValidationError,
+            self.env['contract.contract'].create,
+            [contract_fiber_wrong_tech_args]
+        )
+
+    def test_contract_fiber_wrong_supplier(self):
+        contract_fiber_wrong_supplier_args = self.contract_fiber_args.copy()
+        contract_fiber_wrong_supplier_args['service_supplier_id'] = self.ref(
+            'somconnexio.service_supplier_masmovil'
+        )
+        contract_fiber_wrong_supplier_args['contract_line_ids'] = [(0, False, {
+            "name": "Fiber",
+            "product_id": self.product_broadband_fiber.id
+        }), (0, False, {
+            "name": "One Shot Broadband",
+            "product_id": self.product_broadband_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
+        })]
+        self.assertRaises(
+            ValidationError,
+            self.env['contract.contract'].create,
+            [contract_fiber_wrong_supplier_args]
+        )
+
     def test_contract_fiber_vodafone_allowed_product(self):
         contract_fiber_allowed_product_args = self.contract_fiber_args.copy()
+        contract_fiber_allowed_product_args['service_supplier_id'] = self.ref(
+            'somconnexio.service_supplier_vodafone'
+        )
         contract_fiber_allowed_product_args['contract_line_ids'] = [(0, False, {
             "name": "Fiber",
             "product_id": self.product_broadband_fiber.id
         }), (0, False, {
             "name": "One Shot Broadband",
             "product_id": self.product_broadband_oneshot.id
+        }), (0, False, {
+            "name": "IPv4 Fixa",
+            "product_id": self.product_broadband_additional.id
         })]
         self.assertTrue(
             self.env['contract.contract'].create(contract_fiber_allowed_product_args)
@@ -255,8 +393,11 @@ class ProductCategoryTechnologySupplier(TransactionCase):
 
     def test_contract_fiber_vodafone_wrong_product(self):
         contract_fiber_wrong_product_args = self.contract_fiber_args.copy()
+        contract_fiber_wrong_product_args['service_supplier_id'] = self.ref(
+            'somconnexio.service_supplier_vodafone'
+        )
         contract_fiber_wrong_product_args['contract_line_ids'] = [(0, False, {
-            "name": "Fiber",
+            "name": "ADSL 20Mb",
             "product_id": self.product_broadband_adsl.id
         })]
         self.assertRaises(
